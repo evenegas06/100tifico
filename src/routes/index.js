519f5ea2665c58cfe9c39825e361437680c473ab
Eduardo Venegas Rivera <evenegas06@hotmail.com>
2021-06-05 12:08:11 -0500

    SPA con JavaScript Vanilla

---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+import Header from '../templates/Header';
+import Home from '../pages/Home';
+import Footer from '../templates/Footer';
+import Character from '../pages/Character';
+import Error404 from '../pages/Error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
+
+const routes = {
+  '/': Home,
+  '/:id': Character,
+  '/contact': 'Contact',
+  '/:pages': Home,
+};
+
+const router = async () => {
+  const header = null || document.getElementById('header');
+  const content = null || document.getElementById('content');
+  const footer = null || document.getElementById('footer');
+
+
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  header.innerHTML = await Header();
+  content.innerHTML = await render();
+  footer.innerHTML = await Footer();
+};
+
+export default router;
